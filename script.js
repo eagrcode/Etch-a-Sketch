ab319211ab48685b84a5284d8afe74ad14b22db3
eagrcode <elliot.robinson92@hotmail.com>
2022-09-04 17:07:47 +0100

    fix range slider functionality

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const slider = document.getElementById("grid-slider");
 const sliderLabel = document.getElementById("slider-label");
 
-sliderLabel.innerHTML = slider.value;
-
-// use 'change' instead to see the difference in response
-slider.addEventListener(
-  "input",
-  () => {
-    sliderLabel.innerHTML = i.value;
-  },
-  false
-);
+function displayOutput() {
+  sliderLabel.innerHTML = `${slider.value} x ${slider.value}`;
+}
