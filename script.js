b561f93f2fdd89351b6e8fdc039fe6e23512cc96
eagrcode <elliot.robinson92@hotmail.com>
2022-09-05 14:35:25 +0100

    add playing around with random rgb values

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const slider = document.getElementById("grid-slider");
 const sliderLabel = document.getElementById("slider-label");
 const defaultGridSize = 64;
+let r = Math.floor(Math.random() * 255);
+let g = Math.floor(Math.random() * 255);
+let b = Math.floor(Math.random() * 255);
 
 function defaultGrid(size) {
-  let grid = document.getElementById("grid");
+  const grid = document.getElementById("grid");
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < 128; i++) {
+  for (let i = 0; i < defaultGridSize * defaultGridSize; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = "green";
+    square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     grid.insertAdjacentElement("beforeend", square);
   }
 }
