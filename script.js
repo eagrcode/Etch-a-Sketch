385dd87187006050ed6113b14c454626657a29b3
eagrcode <elliot.robinson92@hotmail.com>
2022-09-05 09:35:44 +0100

    add default grid size

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const slider = document.getElementById("grid-slider");
 const sliderLabel = document.getElementById("slider-label");
+const defaultGridSize = 64;
+
+function defaultGrid(size) {
+  let grid = document.getElementById("grid");
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < 128; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "green";
+    grid.insertAdjacentElement("beforeend", square);
+  }
+}
+
+defaultGrid(defaultGridSize);
 
 function displayOutput() {
   sliderLabel.innerHTML = `${slider.value} x ${slider.value}`;
